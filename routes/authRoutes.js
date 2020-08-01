7bcf0dd851d5a6fecc7e65d7b710ad2cae0b1e18
praveen Dhandapani <praveen.2471993@gmail.com>
2020-08-01 22:40:57 +0530

    Client code

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,22 +1,25 @@
 const passport = require('passport');
 
 module.exports = (app) => {
+    console.log('Inside auth routes');
     app.get('/auth/google', passport.authenticate('google', {
         scope: ['profile', 'email']
     }));
 
-    app.get('/auth/google/callback', passport.authenticate('google'));
+    app.get('/auth/google/callback',
+        passport.authenticate('google'),
+        (req, res) => {
+            res.redirect('/surveys');
+        });
 
     app.get('/api/logout', (req, res) => {
         req.logout();
         res.send(req.user);
     });
 
-    app.get('/api/current_user', (req, res) => {
-        console.log('req >>' + req.user);
-        res.send(req.session);
-       // res.send( req.user );
+    app.get('/api/currentUser', (req, res) => {
+      //  res.send(req.session);
+       res.send( req.user );
     });
-
 
 };
